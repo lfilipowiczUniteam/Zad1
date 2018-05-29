c8fdf7118f2f51707d4ada447e454719efb2ceb1
P.Dudek <paweldominikdudek@gmil.com>
2018-05-29 20:59:01 +0200

    Add input field

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-const App = () => 
-    <div className="app" />
+class App extends  React.Component {
+    state = {
+        searchTerm: ""
+    };
+
+    handleSearchTermChange = event => {
+        this.setState({searchTerm: event.target.value})
+    }
+
+    render() {
+        return (
+        <div className="app">
+            <input type="text" onChange={this.handleSearchTermChange} value={this.state.searchTerm} />
+        </div>
+        )
+    }
+}
 
 
 render(<App />, document.getElementById('app'));
