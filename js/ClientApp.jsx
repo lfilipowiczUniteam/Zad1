3b03379eda31da1a1a9448f52680d2c3c73ff3a4
P.Dudek <paweldominikdudek@gmil.com>
2018-05-29 22:09:25 +0200

    Added componentDidMount()

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import { render } from 'react-dom'
+import axios from 'axios'
 
 class App extends  React.Component {
     state = {
-        searchTerm: ""
+        searchTerm: "",
+        data: [{
+            userId: '',
+            id: '',
+            title: '',
+            body: ''
+        }]
     };
+
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.data.map(({userId, id, title, body}) => ({userId, id, title, body})))
+        .then(data => this.setState({data: data}))
+    }
 
     handleSearchTermChange = event => {
         this.setState({searchTerm: event.target.value})
@@ -12,7 +25,7 @@ class App extends  React.Component {
 
     render() {
         return (
-        <div className="app">
+        <div className='app'>
             <input type="text" onChange={this.handleSearchTermChange} value={this.state.searchTerm} />
         </div>
         )
